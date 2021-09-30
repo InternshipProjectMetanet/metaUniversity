fed780f2b13e7b458acf96190a586ceb98a78bb2
spammy7147 <spammy7147@gmail.com>
2021-09-30 10:06:55 +0900

    네비바 사용자 이미지

--- src/main/java/com/example/metauniversity/controller/NavController.java
+++ src/main/java/com/example/metauniversity/controller/NavController.java
@@ -1,6 +1,8 @@
 package com.example.metauniversity.controller;
 
 import com.example.metauniversity.security.CustomUserDetails;
+import com.example.metauniversity.service.NavService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 
 @RestController
-public class TestController {
+@RequiredArgsConstructor
+public class NavController {
 
-    @GetMapping("/test")
-    public Principal auth(Principal principal){
-        return principal.;
+    private NavService navService;
+
+    @GetMapping("/get/user/icon")
+    public String auth(@AuthenticationPrincipal CustomUserDetails currentUser){
+
+        if (currentUser == null){
+            return "/img/account_circle.svg";
+        }else {
+            return navService.getUserImg(currentUser);
+        }
 
     }
 }
